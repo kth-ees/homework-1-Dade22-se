b771fac4b458d8483da89006c7fdc442920c51af
Davide <davidevi@kth.se>
2025-09-10 15:21:38 +0200

    bin2bcd_v1
    first implementation of the bin2bcd module

--- q2/bin2bcd.sv
+++ q2/bin2bcd.sv
@@ -3,7 +3,33 @@ module bin2bcd (
   output logic [3:0] bcd,
   output logic carry
 );
-  // …
-  // Add your description here
-  // …
+
+always_comb begin
+  
+  bcd = 4'b0000;
+  carry = 1'b0;
+
+  case (binary)
+    4'b0000: bcd = 4'b0000;// 0
+    4'b0001: bcd = 4'b0001; // 1
+    4'b0010: bcd = 4'b0010; // 2
+    4'b0011: bcd = 4'b0011; // 3
+    4'b0100: bcd = 4'b0100; // 4
+    4'b0101: bcd = 4'b0101; // 5
+    4'b0110: bcd = 4'b0110; // 6
+    4'b0111: bcd = 4'b0111; // 7
+    4'b1000: bcd = 4'b1000; // 8
+    4'b1001: bcd = 4'b1001; // 9
+    4'b1010: begin bcd = 4'b0000; carry = 1'b1; end// 10 -> 0 with carry
+    4'b1011: begin bcd = 4'b0001; carry = 1'b1; end// 11 -> 1 with carry
+    4'b1100: begin bcd = 4'b0010; carry = 1'b1; end// 12 -> 2 with carry
+    4'b1101: begin bcd = 4'b0011; carry = 1'b1; end// 13 -> 3 with carry
+    4'b1110: begin bcd = 4'b0100; carry = 1'b1; end// 14 -> 4 with carry
+    4'b1111: begin bcd = 4'b0101; carry = 1'b1; end// 15 -> 5 with carry
+    default: begin
+      bcd = 4'b0000;
+      carry = 1'b0;
+    end
+end
+
 endmodule
--- q2/bin2bcd_tb.sv
+++ q2/bin2bcd_tb.sv
@@ -5,5 +5,18 @@ module bin2bcd_tb;
   logic [3:0] bcd;
   logic carry;
 
-  // complete your tb
+  // Instantiate the DUT (Device Under Test)
+  bin2bcd dut (
+    .binary(binary),
+    .bcd(bcd),
+    .carry(carry)
+  );
+
+  // Test procedure
+  for (int i=0; i<10; i++) begin
+    binary = $urandom_range(0, 15); // Random binary input from 0 to 15
+    #10; // Wait for 10 time units
+    $display("Binary: %b, BCD: %b, Carry: %b", binary, bcd, carry);
+  end
+
 endmodule
